6a6d5cacc1e7c1acd54c2c71cfe533ebf71b4580
Nicole Dicochea <dicocheanicole@gmail.com>
2025-08-23 17:39:32 -0700

    update vite config for redirects

--- pinterest-randomizer/vite.config.ts
+++ pinterest-randomizer/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copy } from 'fs-extra'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-redirects',
+      writeBundle() {
+        copy(
+          resolve(__dirname, 'client/_redirects'),
+          resolve(__dirname, 'dist/_redirects')
+        )
+      }
+    }
+  ],
 })
